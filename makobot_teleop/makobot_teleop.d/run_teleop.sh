096e04247d83b6e9c21f5cb44c4357ee728e48e4
awilby <awilby@ucsd.edu>
2018-05-23 11:24:51 -0700

    update username on rpi0

--- makobot_teleop/makobot_teleop.d/run_teleop.sh
+++ makobot_teleop/makobot_teleop.d/run_teleop.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-bash -c "source /home/pi/ros_ws/devel/setup.bash && roslaunch makobot_teleop teleop.launch"
+bash -c "source /home/mako/ros_ws/devel/setup.bash && roslaunch makobot_teleop teleop.launch"
